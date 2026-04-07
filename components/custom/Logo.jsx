b2ad8f54e9d64ab59cbb59d9538c2430ece285e7
AvatarN03 <jsprashanth003@gmail.com>
2026-04-07 14:14:16 +0530

    temp: name change

--- components/custom/Logo.jsx
+++ components/custom/Logo.jsx
@@ -12,8 +12,8 @@ const Logo = () => {
       height={28} 
       />
       <h1 className="text-base md:text-2xl font-black bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent hover:text-slate-200 transition-all group-hover:scale-105 ">
-        {/* AI Tour */}
-        Prava AI
+        AI Tour
+        {/* Prava AI */}
       </h1>
     </Link>
   );
